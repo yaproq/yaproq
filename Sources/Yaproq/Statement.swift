8ce5b48d1ea285036e1842449223a19ce1770921
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-11-22 16:34:39 +0500

    Create BlockStatement class

--- Sources/Yaproq/Statement.swift
+++ Sources/Yaproq/Statement.swift
@@ -2,4 +2,20 @@ protocol Statement {
     func accept(visitor: StatementVisitor) throws
 }
 
-protocol StatementVisitor {}
+protocol StatementVisitor {
+    func visitBlock(statement: BlockStatement) throws
+}
+
+class BlockStatement: Statement {
+    let name: String?
+    var statements: [Statement]
+
+    init(name: String? = nil, statements: [Statement]) {
+        self.name = name
+        self.statements = statements
+    }
+
+    func accept(visitor: StatementVisitor) throws {
+        try visitor.visitBlock(statement: self)
+    }
+}
