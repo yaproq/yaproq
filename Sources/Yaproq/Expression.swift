e0d33c462f643d4f7bc32fb5bce29f2e8ccde79c
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-11-22 16:04:13 +0500

    Create AssignmentExpression class

--- Sources/Yaproq/Expression.swift
+++ Sources/Yaproq/Expression.swift
@@ -2,4 +2,20 @@ protocol Expression {
     func accept(visitor: ExpressionVisitor) throws -> Any?
 }
 
-protocol ExpressionVisitor {}
+protocol ExpressionVisitor {
+    func visitAssignment(expression: AssignmentExpression) throws -> Any?
+}
+
+class AssignmentExpression: Expression {
+    let token: Token
+    let value: Expression
+
+    init(token: Token, value: Expression) {
+        self.token = token
+        self.value = value
+    }
+
+    func accept(visitor: ExpressionVisitor) throws -> Any? {
+        try visitor.visitAssignment(expression: self)
+    }
+}
