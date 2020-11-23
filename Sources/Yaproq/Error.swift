dc198e8afa512a54eee81387997914e8ffee69a8
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-11-23 19:38:56 +0500

    Create RuntimeError

--- Sources/Yaproq/Error.swift
+++ Sources/Yaproq/Error.swift
@@ -34,3 +34,25 @@ public struct SyntaxError: LocalizedError {
         }
     }
 }
+
+public struct RuntimeError: LocalizedError {
+    let message: String?
+    let line: Int?
+    let column: Int?
+    public var errorDescription: String? { message }
+
+    init(_ message: String? = nil, line: Int? = nil, column: Int? = nil) {
+        self.line = line
+        self.column = column
+
+        if let line = line, let column = column {
+            if let message = message {
+                self.message = "[Line: \(line), Column: \(column)] Runtime error: \(message)"
+            } else {
+                self.message = "[Line: \(line), Column: \(column)] Runtime error: Unknown error."
+            }
+        } else {
+            self.message = message
+        }
+    }
+}
