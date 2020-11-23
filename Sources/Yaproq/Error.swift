f32aa439600fdc20c268ba2c56629ef4d2bfa6c2
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-11-23 19:38:41 +0500

    Create SyntaxError

--- Sources/Yaproq/Error.swift
+++ Sources/Yaproq/Error.swift
@@ -12,3 +12,25 @@ public struct YaproqError: LocalizedError {
         }
     }
 }
+
+public struct SyntaxError: LocalizedError {
+    let message: String?
+    let line: Int?
+    let column: Int?
+    public var errorDescription: String? { message }
+
+    init(_ message: String? = nil, line: Int? = nil, column: Int? = nil) {
+        self.line = line
+        self.column = column
+
+        if let line = line, let column = column {
+            if let message = message {
+                self.message = "[Line: \(line), Column: \(column)] Syntax error: \(message)"
+            } else {
+                self.message = "[Line: \(line), Column: \(column)] Syntax error: Unknown error."
+            }
+        } else {
+            self.message = message
+        }
+    }
+}
