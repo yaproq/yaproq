9f59e9e567aadf7a4a622308229f7784b1aaefa4
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-11-25 19:38:07 +0500

    Test Token init

--- Tests/YaproqTests/TokenTests.swift
+++ Tests/YaproqTests/TokenTests.swift
@@ -1,4 +1,20 @@
 @testable import Yaproq
 import XCTest
 
-final class TokenTests: XCTestCase {}
+final class TokenTests: XCTestCase {
+    func testInit() {
+        // Arrange
+        let kind = Token.Kind.and
+        let line = 1
+        let column = 3
+
+        // Act
+        let token = Token(kind: kind, lexeme: kind.rawValue, line: line, column: column)
+
+        // Assert
+        XCTAssertEqual(token.kind, kind)
+        XCTAssertEqual(token.lexeme, kind.rawValue)
+        XCTAssertEqual(token.line, line)
+        XCTAssertEqual(token.column, column)
+    }
+}
