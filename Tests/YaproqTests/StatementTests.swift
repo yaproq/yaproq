11f21a75bc37ba80fcb6bf2a160106e3f12f019e
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-11-30 14:34:43 +0500

    Add BlockStatementTests

--- Tests/YaproqTests/StatementTests.swift
+++ Tests/YaproqTests/StatementTests.swift
@@ -1,6 +1,29 @@
 import XCTest
 @testable import Yaproq
 
+final class BlockStatementTests: XCTestCase {
+    func testInit() {
+        // Act
+        var statement = BlockStatement()
+
+        // Assert
+        XCTAssertNil(statement.name)
+        XCTAssertTrue(statement.statements.isEmpty)
+
+        // Arrange
+        let name = "title"
+        let expression = LiteralExpression(token: .init(kind: .number, lexeme: "1", literal: 1, line: 1, column: 4))
+
+        // Act
+        statement = BlockStatement(name: name, statements: [PrintStatement(expression: expression)])
+
+        // Assert
+        XCTAssertEqual(statement.name, name)
+        XCTAssertEqual(statement.statements.count, 1)
+        XCTAssertEqual((statement.statements.first as! PrintStatement).expression as! LiteralExpression, expression)
+    }
+}
+
 final class ExpressionStatementTests: XCTestCase {
     func testInit() {
         // Arrange
