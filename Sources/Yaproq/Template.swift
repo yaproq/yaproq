a1a70b6acfecfc09ea73694ac83e067e451ebb70
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-01-27 22:09:28 +0500

    Make Template conform to CustomStringConvertible

--- Sources/Yaproq/Template.swift
+++ Sources/Yaproq/Template.swift
@@ -1,6 +1,7 @@
-public struct Template: Equatable, ExpressibleByStringLiteral {
+public struct Template: CustomStringConvertible, Equatable, ExpressibleByStringLiteral {
     public let source: String
     public private(set) var filePath: String?
+    public var description: String { source }
 
     public init(stringLiteral source: String) {
         self.source = source
