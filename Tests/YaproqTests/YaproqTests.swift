60474742c82c95b55956f4fff0d94d03bd1190b3
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-02-27 16:41:08 +0500

    Add test

--- Tests/YaproqTests/YaproqTests.swift
+++ Tests/YaproqTests/YaproqTests.swift
@@ -51,15 +51,151 @@ final class YaproqConfigurationTests: BaseTests {
 }
 
 final class YaproqTests: BaseTests {
-    func testInit() {
+    var templating: Yaproq!
+
+    override func setUp() {
+        super.setUp()
+
+        let configuration = Yaproq.Configuration(directoryPath: Bundle.module.resourcePath!)
+        templating = Yaproq(configuration: configuration)
+    }
+
+    func testForStatement() {
         // Arrange
-        let configuration = Yaproq.Configuration()
+        let min = 0
+        let max = 3
+        var template = Template("""
+        {% for value in \(min)...\(max) %}
+        {{ value }}
+        {% endfor %}
+        """
+        )
 
         // Act
-        let templating = Yaproq(configuration: configuration)
+        var result = try! templating.renderTemplate(template)
 
         // Assert
-        XCTAssertEqual(templating.configuration.directoryPath, configuration.directoryPath)
-        XCTAssertNotNil(templating.environment)
+        XCTAssertEqual(result, "0123")
+
+        // Arrange
+        template = Template("""
+        {% for value in \(min)..<\(max) %}
+        {{ value }}
+        {% endfor %}
+        """
+        )
+
+        // Act
+        result = try! templating.renderTemplate(template)
+
+        // Assert
+        XCTAssertEqual(result, "012")
+
+        // Arrange
+        template = Template("""
+        {% for value in min...max %}
+        {{ value }}
+        {% endfor %}
+        """
+        )
+
+        // Act
+        result = try! templating.renderTemplate(template, in: ["min": min, "max": max])
+
+        // Assert
+        XCTAssertEqual(result, "0123")
+
+        // Arrange
+        template = Template("""
+        {% for value in min..<max %}
+        {{ value }}
+        {% endfor %}
+        """
+        )
+
+        // Act
+        result = try! templating.renderTemplate(template, in: ["min": min, "max": max])
+
+        // Assert
+        XCTAssertEqual(result, "012")
+
+        // Arrange
+        template = Template("""
+        {% var min = \(min) %}
+        {% var max = \(max) %}
+        {% for value in min...max %}
+        {{ value }}
+        {% endfor %}
+        """
+        )
+
+        // Act
+        result = try! templating.renderTemplate(template)
+
+        // Assert
+        XCTAssertEqual(result, "0123")
+
+        // Arrange
+        template = Template("""
+        {% var min = \(min) %}
+        {% var max = \(max) %}
+        {% for value in min..<max %}
+        {{ value }}
+        {% endfor %}
+        """
+        )
+
+        // Act
+        result = try! templating.renderTemplate(template)
+
+        // Assert
+        XCTAssertEqual(result, "012")
+
+        // Arrange
+        let array = [1, 2, 3]
+        template = Template("""
+        {% for item in array %}
+        {{ item }}
+        {% endfor %}
+        """
+        )
+
+        // Act
+        result = try! templating.renderTemplate(template, in: ["array": array])
+
+        // Assert
+        XCTAssertEqual(result, "123")
+
+        // Arrange
+        template = Template("""
+        {% for index, value in array %}
+        {{ index }}-{{ value }}
+        {% endfor %}
+        """
+        )
+
+        // Act
+        result = try! templating.renderTemplate(template, in: ["array": array])
+
+        // Assert
+        XCTAssertEqual(result, "0-11-22-3")
+
+        // Arrange
+        let dictionary = ["one": 1, "two": 2, "three": 3]
+        template = Template("""
+        {% for key, value in dictionary %}
+        {{ key }}-{{ value }}
+        {% endfor %}
+        """
+        )
+
+        // Act
+        result = try! templating.renderTemplate(template, in: ["dictionary": dictionary])
+
+        // Assert
+        XCTAssertEqual(result.count, 17)
+        XCTAssertTrue(result.contains("one-1"))
+        XCTAssertTrue(result.contains("two-2"))
+        XCTAssertTrue(result.contains("three-3"))
     }
 }
