f3bac8cdd6ff620af493afd0506b6a805b7cda48
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-05-02 12:03:33 +0500

    Test assigning and defining variables of Environment

--- Tests/YaproqTests/EnvironmentTests.swift
+++ Tests/YaproqTests/EnvironmentTests.swift
@@ -16,48 +16,54 @@ final class EnvironmentTests: BaseTests {
         XCTAssertTrue(childEnvironment.parent === parentEnvironment)
     }
 
-    func testSetVariable() {
+    func testAssignAndDefineVariable() {
         // Arrange
-        let environment = Environment()
-        let name = "title"
-        let value = "Home"
-        let token = Token(kind: .identifier, lexeme: name, literal: value, line: 1, column: 8)
-
-        // Act
-        environment.setVariable(value: value, for: name)
-
-        // Assert
-        XCTAssertEqual(try? environment.getVariableValue(for: token) as? String, value)
-    }
-
-    func testDefineVariable() {
-        // Arrange
-        let environment = Environment()
+        let value1 = "value1"
+        let value2 = "value2"
+        let value3 = "value3"
+        let token = Token(kind: .identifier, lexeme: "name", literal: value1, line: -1, column: -1)
+        let parentEnvironment = Environment()
+        let childEnvironment = Environment(parent: parentEnvironment)
 
         // Act/Assert
-        XCTAssertNoThrow(
-            try environment.defineVariable(
-                value: "Home",
-                for: .init(kind: .string, lexeme: "title", line: 1, column: 12)
+        XCTAssertThrowsError(try parentEnvironment.assignVariable(value: token.literal, for: token)) { error in
+            guard let error = error as? RuntimeError else {
+                XCTFail("The error is not of \(String(describing: RuntimeError.self)) type.")
+                return
+            }
+
+            XCTAssertNil(error.filePath)
+            XCTAssertEqual(error.line, -1)
+            XCTAssertEqual(error.column, -1)
+            XCTAssertEqual(error.errorDescription, """
+            [Line: \(error.line), Column: \(error.column)] \
+            RuntimeError: An undefined variable '\(token.lexeme)'.
+            """
             )
-        )
-        XCTAssertThrowsError(
-            try environment.defineVariable(
-                value: "Home",
-                for: .init(kind: .string, lexeme: "title", line: 2, column: 12)
+        }
+        XCTAssertNoThrow(try parentEnvironment.defineVariable(value: token.literal, for: token))
+        XCTAssertThrowsError(try parentEnvironment.defineVariable(value: token.literal, for: token)) { error in
+            guard let error = error as? RuntimeError else {
+                XCTFail("The error is not of \(String(describing: RuntimeError.self)) type.")
+                return
+            }
+
+            XCTAssertNil(error.filePath)
+            XCTAssertEqual(error.line, -1)
+            XCTAssertEqual(error.column, -1)
+            XCTAssertEqual(error.errorDescription, """
+            [Line: \(error.line), Column: \(error.column)] \
+            RuntimeError: A variable '\(token.lexeme)' already exists.
+            """
             )
-        )
-    }
-
-    func testAssignValueToVariable() {
-        // Arrange
-        let environment = Environment()
-
-        let token = Token(kind: .string, lexeme: "title", line: 1, column: 12)
-
-        // Act/Assert
-        XCTAssertNoThrow(try environment.defineVariable(value: "Home", for: token))
-        XCTAssertNil(environment.parent)
-        XCTAssertThrowsError(try environment.defineVariable(value: "Home", for: token))
+        }
+        XCTAssertEqual(try? parentEnvironment.getVariableValue(for: token) as? String, value1)
+        XCTAssertEqual(try? childEnvironment.getVariableValue(for: token) as? String, value1)
+        XCTAssertNoThrow(try childEnvironment.assignVariable(value: value2, for: token))
+        XCTAssertEqual(try? parentEnvironment.getVariableValue(for: token) as? String, value1)
+        XCTAssertEqual(try? childEnvironment.getVariableValue(for: token) as? String, value2)
+        XCTAssertNoThrow(try childEnvironment.assignVariable(value: value3, for: token))
+        XCTAssertEqual(try? parentEnvironment.getVariableValue(for: token) as? String, value1)
+        XCTAssertEqual(try? childEnvironment.getVariableValue(for: token) as? String, value3)
     }
 }
