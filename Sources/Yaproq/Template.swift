5fbd0f99246a63f5b9a36dbf5e5f24721ab96785
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-05-13 18:45:32 +0500

    Refactor Template

--- Sources/Yaproq/Template.swift
+++ Sources/Yaproq/Template.swift
@@ -1,10 +1,11 @@
 public struct Template: CustomStringConvertible, Equatable, ExpressibleByStringLiteral {
     public let source: String
-    public private(set) var filePath: String?
+    public let filePath: String?
     public var description: String { source }
 
     public init(stringLiteral source: String) {
         self.source = source
+        filePath = nil
     }
 
     public init(_ source: String, filePath: String? = nil) {
