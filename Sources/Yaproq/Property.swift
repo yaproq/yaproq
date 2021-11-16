8ccebe726280d1bfc9dd701945ddd1429c8574cc
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-11-16 22:33:17 +0500

    Refactor Property

--- Sources/Yaproq/Property.swift
+++ Sources/Yaproq/Property.swift
@@ -18,53 +18,41 @@ enum Property: String, CaseIterable {
         switch self {
         case .capitalized:
             if let value = value as? String { return value.capitalized }
-            throw runtimeError(.undefinedVariableOrProperty(rawValue), token: token)
         case .capitalizeFirstCharacter:
             if let value = value as? String { return value.capitalizeFirstCharacter }
-            throw runtimeError(.undefinedVariableOrProperty(rawValue), token: token)
         case .count:
             if let value = value as? [Any] { return value.count }
             if let value = value as? [AnyHashable: Any] { return value.count }
             if let value = value as? String { return value.count }
-            throw runtimeError(.undefinedVariableOrProperty(rawValue), token: token)
         case .first:
             if let value = value as? [Any] { return value.first }
             if let value = value as? String { return value.first }
-            throw runtimeError(.undefinedVariableOrProperty(rawValue), token: token)
         case .isEmpty:
             if let value = value as? [Any] { return value.isEmpty }
             if let value = value as? [AnyHashable: Any] { return value.isEmpty }
             if let value = value as? String { return value.isEmpty }
-            throw runtimeError(.undefinedVariableOrProperty(rawValue), token: token)
         case .keys:
             if let value = value as? [AnyHashable: Any] { return Array(value.keys) }
-            throw runtimeError(.undefinedVariableOrProperty(rawValue), token: token)
         case .last:
             if let value = value as? [Any] { return value.last }
             if let value = value as? String { return value.last }
-            throw runtimeError(.undefinedVariableOrProperty(rawValue), token: token)
         case .localizedCapitalized:
             if let value = value as? String { return value.localizedCapitalized }
-            throw runtimeError(.undefinedVariableOrProperty(rawValue), token: token)
         case .localizedLowercase:
             if let value = value as? String { return value.localizedLowercase }
-            throw runtimeError(.undefinedVariableOrProperty(rawValue), token: token)
         case .localizedUppercase:
             if let value = value as? String { return value.localizedUppercase }
-            throw runtimeError(.undefinedVariableOrProperty(rawValue), token: token)
         case .lowercased:
             if let value = value as? String { return value.lowercased() }
-            throw runtimeError(.undefinedVariableOrProperty(rawValue), token: token)
         case .reversed:
             if let value = value as? [Any] { return Array(value.reversed()) }
             if let value = value as? String { return String(value.reversed()) }
-            throw runtimeError(.undefinedVariableOrProperty(rawValue), token: token)
         case .uppercased:
             if let value = value as? String { return value.uppercased() }
-            throw runtimeError(.undefinedVariableOrProperty(rawValue), token: token)
         case .values:
             if let value = value as? [AnyHashable: Any] { return Array(value.values) }
-            throw runtimeError(.undefinedVariableOrProperty(rawValue), token: token)
         }
+
+        throw runtimeError(.undefinedVariableOrProperty(rawValue), token: token)
     }
 }
