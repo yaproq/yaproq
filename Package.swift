0e9839625a54ab07f087793584bd20585729ea7b
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2022-04-21 22:42:05 +0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,14 +4,22 @@ import PackageDescription
 
 let package = Package(
     name: "yaproq",
-    platforms: [.macOS(.v10_11)],
-    products: [.library(name: "Yaproq", targets: ["Yaproq"])],
+    platforms: [
+        .macOS(.v10_11)
+    ],
+    products: [
+        .library(name: "Yaproq", targets: ["Yaproq"])
+    ],
     targets: [
         .target(name: "Yaproq"),
         .testTarget(
             name: "YaproqTests",
-            dependencies: [.target(name: "Yaproq")],
-            resources: [.process("Resources")]
+            dependencies: [
+                .target(name: "Yaproq")
+            ],
+            resources: [
+                .process("Resources")
+            ]
         )
     ],
     swiftLanguageVersions: [.v5]
