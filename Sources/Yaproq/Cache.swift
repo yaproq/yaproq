e409acbaf97498f7d6e653d73a5f1141d4fc9576
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2023-04-30 13:07:01 +0500

    Rename cache to underlyingCache

--- Sources/Yaproq/Cache.swift
+++ Sources/Yaproq/Cache.swift
@@ -2,16 +2,16 @@ import Foundation
 
 final class Cache<Key: Hashable, Value> {
     var costLimit: Int {
-        get { cache.totalCostLimit }
-        set { cache.totalCostLimit = newValue }
+        get { underlyingCache.totalCostLimit }
+        set { underlyingCache.totalCostLimit = newValue }
     }
 
     var countLimit: Int {
-        get { cache.countLimit }
-        set { cache.countLimit = newValue }
+        get { underlyingCache.countLimit }
+        set { underlyingCache.countLimit = newValue }
     }
 
-    private var cache = NSCache<NSNumber, CachedItem<Value>>()
+    private var underlyingCache = NSCache<NSNumber, CachedItem<Value>>()
 
     init(costLimit: Int = 0, countLimit: Int = 0) {
         self.costLimit = costLimit
@@ -21,29 +21,29 @@ final class Cache<Key: Hashable, Value> {
     func setValue(_ value: Value, forKey key: Key) {
         let key = NSNumber(value: key.hashValue)
         let item = CachedItem(value: value)
-        cache.setObject(item, forKey: key)
+        underlyingCache.setObject(item, forKey: key)
     }
 
     func setValue(_ value: Value, forKey key: Key, cost: Int) {
         let key = NSNumber(value: key.hashValue)
         let item = CachedItem(value: value)
-        cache.setObject(item, forKey: key, cost: cost)
+        underlyingCache.setObject(item, forKey: key, cost: cost)
     }
 
     func getValue(forKey key: Key) -> Value? {
         let key = NSNumber(value: key.hashValue)
-        guard let item = cache.object(forKey: key) else { return nil }
+        guard let item = underlyingCache.object(forKey: key) else { return nil }
 
         return item.value
     }
 
     func removeValue(forKey key: String) {
         let key = NSNumber(value: key.hashValue)
-        cache.removeObject(forKey: key)
+        underlyingCache.removeObject(forKey: key)
     }
 
     func clear() {
-        cache.removeAllObjects()
+        underlyingCache.removeAllObjects()
     }
 }
 
